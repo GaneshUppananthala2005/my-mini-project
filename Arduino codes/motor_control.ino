e6343f40fced4049a374bbff82e78c127a5e373b
GaneshUppananthala2005 <uganeshuppananthala@gmail.com>
2026-01-31 10:43:39 +0530

    Add motor control functions for movement

---
+++ Arduino codes/motor_control.ino
@@ -0,0 +1,16 @@
+#include "motor_control.h"
+#include "pin_config.h"
+
+void moveForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
